39c441af6942fc5ef4743ef8bef3a06b1945c2e4
kim <kamimong@naver.com>
2019-04-27 17:05:10 +0900

    yy_circle

--- RealCodingHW/src/main/java/Calculator.java
+++ RealCodingHW/src/main/java/Calculator.java
@@ -18,5 +18,9 @@ public class Calculator {
     public void wonToDollar() { dollar = getWon() / 1200; }
     public void wonToPound() { pound = getWon() / 1500; }
     public void wonToYen() { yen = getWon() / 10; }
+    //************************************yy************************************
+    public double circle(int a){
+        return a*a*3.14;
+    }
 
 }
